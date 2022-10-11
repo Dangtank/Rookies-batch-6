6a23eb3f13bf15b2292b81f3230dfbba977bb623
Dangtank <dangtuananh_t63@hus.edu.vn>
2022-10-11 23:58:43 +0700

    upload C# Dotnet Core MVC Day2

---
+++ PHASE 1/Dotnet_Core_MVC/Day2/Controllers/RookiesController.cs
@@ -0,0 +1,181 @@
+using Day2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day2.Controllers
+{
+    public class RookiesController : Controller
+    {
+        private readonly ILogger<RookiesController> _logger;
+
+        public RookiesController(ILogger<RookiesController> logger)
+        {
+            _logger = logger;
+        }
+
+        private static List<PersonModel> _people = new List<PersonModel>
+        {
+            new PersonModel
+            {
+                FirstName = "Giang",
+                LastName = "Vu Hoang",
+                Gender = "Male",
+                DateOfBirth = new DateTime(2000, 3, 15),
+                PhoneNumber = "",
+                BirthPlace = "Phu Tho",
+                IsGraduated = false
+            },
+            new PersonModel
+            {
+                FirstName = "Kien",
+                LastName = "Mai trung",
+                Gender = "Male",
+                DateOfBirth = new DateTime(2001, 3, 15),
+                PhoneNumber = "",
+                BirthPlace = "Thanh Hoa",
+                IsGraduated = false
+            },
+            new PersonModel
+            {
+                FirstName = "Kiet",
+                LastName = "Hoang Duc",
+                Gender = "Male",
+                DateOfBirth = new DateTime(2002, 3, 15),
+                PhoneNumber = "",
+                BirthPlace = "Nam Dinh",
+                IsGraduated = false
+            },
+            new PersonModel
+            {
+                FirstName = "Trong",
+                LastName = "Le Quy",
+                Gender = "Female",
+                DateOfBirth = new DateTime(2003, 3, 15),
+                PhoneNumber = "",
+                BirthPlace = "Hung Yen",
+                IsGraduated = false
+            },
+            new PersonModel
+            {
+                FirstName = "Hieu",
+                LastName = "Hoang Trung",
+                Gender = "Male",
+                DateOfBirth = new DateTime(2002, 3, 15),
+                PhoneNumber = "",
+                BirthPlace = "Ha Noi",
+                IsGraduated = false
+            },
+            new PersonModel
+            {
+                FirstName = "Hoan",
+                LastName = "Nguyen Van",
+                Gender = "Male",
+                DateOfBirth = new DateTime(2000, 3, 15),
+                PhoneNumber = "",
+                BirthPlace = "Quang Ninh",
+                IsGraduated = false
+            },
+            new PersonModel
+            {
+                FirstName = "Anh",
+                LastName = "Dang Tuan",
+                Gender = "Male",
+                DateOfBirth = new DateTime(2000, 3, 15),
+                PhoneNumber = "",
+                BirthPlace = "Ninh Binh",
+                IsGraduated = false
+            },
+        };
+
+        public IActionResult Index()
+        {
+            return View(_people);
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(PersonModel personModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var person = new PersonModel
+                {
+                    FirstName = personModel.FirstName,
+                    LastName = personModel.LastName,
+                    Gender = personModel.Gender,
+                    DateOfBirth = personModel.DateOfBirth,
+                    BirthPlace = personModel.BirthPlace,
+                    PhoneNumber = personModel.PhoneNumber,
+                    IsGraduated = true
+                };
+                _people.Add(person);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(personModel);
+        }
+
+        [HttpGet]
+
+        public IActionResult Edit(int index)
+        {
+            if (index >= 0 && index < _people.Count)
+            {
+                var person = _people[index];
+                var personModelUpdate = new PersonModelUpdate
+                {
+                    FirstName = person.FirstName,
+                    LastName = person.LastName,
+                    Gender = person.Gender,
+                    DateOfBirth = person.DateOfBirth,
+                    BirthPlace = person.BirthPlace,
+                    PhoneNumber = person.PhoneNumber,
+                };
+
+                return View(personModelUpdate);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Update(int index, PersonModelUpdate personModelUpdate)
+        {
+            if (ModelState.IsValid)
+            {
+                if (index >= 0 && index < _people.Count)
+                {
+                    var person = _people[index];
+                    person.FirstName = personModelUpdate.FirstName;
+                    person.LastName = personModelUpdate.LastName;
+                    person.PhoneNumber = personModelUpdate.PhoneNumber;
+                    person.BirthPlace = personModelUpdate.BirthPlace;
+                }
+
+                return RedirectToAction("Index");
+            }
+            
+            return View(personModelUpdate);
+        }
+
+        [HttpPost]
+
+        public IActionResult Delete(int index)
+        {
+            if (index >= 0 && index < _people.Count)
+            {
+                _people.RemoveAt(index);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PHASE 1/Dotnet_Core_MVC/Day2/Models/PersonModelUpdate.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day2.Models
+{
+    public class PersonModelUpdate
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? BirthPlace { get; set; }
+    }
+}
